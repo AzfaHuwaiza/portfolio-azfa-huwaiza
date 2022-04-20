78fed327c963aaf79284de752bff92e6ef3ef809
azfa <aztmin55@gmail.com>
2022-04-21 02:37:15 +0700

    some init msg

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    extend: {
+      colors: {
+        primary: '#60a5fa',
+        secondary: '#64748b',
+        dark: '#171717',
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
